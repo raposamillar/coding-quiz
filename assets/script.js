f865839fe4441f8a0a752d6ddc32a466a9da2dd7
raposamillar <lisa.raposamillar@gmail.com>
2022-07-24 07:26:48 -0400

    edit HTML; add initial JavaScript

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// Create semantic HTML
+var body = document.body;
+var h1El = document.createElement("h1");
+
+// Create text content
+h1El.textContent = "Coding Quiz: JavaScript";
+
+// Create appendChild
+body.appendChild(h1El);
+
+// Set attributes
+h1El.setAttribute("style", "margin:auto; width:50%; text-align:center;");
