6f49a69d6e95c75a80f1f6b00b3be419da6379e2
raposamillar <lisa.raposamillar@gmail.com>
2022-07-28 13:43:27 -0400

    set up html, css, and js

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,34 @@
-// Create semantic HTML
-var body = document.body;
-var h1El = document.createElement("h1");
+// QUIZ INFORMATION / VARIABLES
+// ARRAY
+// Variable containing the array of questions, choices, and answers
+var questions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    question: "The condition in an if/else statement is enclosed within __________.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store __________.",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above",
+  },
+  {
+    question: "String values must be enclosed within __________ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes",
+  },
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    answer: "console.log",
+  },
+];
+// END QUIZ INFORMATION 
 
-// Create text content
-h1El.textContent = "Coding Quiz: JavaScript";
-
-// Create appendChild
-body.appendChild(h1El);
-
-// Set attributes
-h1El.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+// RUN QUIZ
+startQuiz();
