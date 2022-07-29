7a5c4ed7d2e3487f8395eca1dfc8e16f9002d9c0
raposamillar <lisa.raposamillar@gmail.com>
2022-07-28 22:07:36 -0400

    restructure js

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,59 @@
-// Create semantic HTML
-var body = document.body;
-var h1El = document.createElement("h1");
+var questions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    question: "The condition in an if/else statement is enclosed within __________.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store __________.",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above",
+  },
+  {
+    question: "String values must be enclosed within __________ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes",
+  },
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    answer: "console.log",
+  },
+];
 
-// Create text content
-h1El.textContent = "Coding Quiz: JavaScript";
+var mainEl = document.querySelector("main");
 
-// Create appendChild
-body.appendChild(h1El);
+var startQuiz = function(e) {
+  if (e.target.matches("#quiz-start")) {
+    document.querySelector("#start-frame").remove ();
+    
+    var sectionEl = document.createElement("section");
+    sectionEl.className = "questions";
+    sectionEl.setAttribute("id", "question-frame");
+    
+    var questionTitle = document.createElement("h1");
+    questionTitle.textContent = questions[0].question;
+    sectionEl.appendChild(questionTitle);
+    mainEl.appendChild(sectionEl);
+  
+    var choicesPossible = document.createElement("ol");
 
-// Set attributes
-h1El.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+    var choicesPossible = document.createElement("li");
+    choicesPossible.textContent = questions[0].choices[0];
+    sectionEl.className = "questions";
+    sectionEl.setAttribute("id", "choices");
+    sectionEl.appendChild(choicesPossible);
+    mainEl.appendChild(sectionEl);
+
+    console.log(sectionEl);
+    console.log(questionTitle);
+    console.log(choicesPossible);
+
+  }
+};
+mainEl.addEventListener("click", startQuiz)
