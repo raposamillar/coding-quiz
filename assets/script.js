df7f59155d966a6253388c9172d152104f586d7b
raposamillar <lisa.raposamillar@gmail.com>
2022-07-28 21:57:52 -0400

    add startQuiz function

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,3 @@
-// QUIZ INFORMATION / VARIABLES
-// ARRAY
 // Variable containing the array of questions, choices, and answers
 var questions = [
   {
@@ -28,7 +26,35 @@ var questions = [
     answer: "console.log",
   },
 ];
-// END QUIZ INFORMATION 
 
-// RUN QUIZ
-startQuiz();
+var mainEl = document.querySelector("main");
+
+var startQuiz = function(e) {
+  if (e.target.matches("#quiz-start")) {
+    document.querySelector("#start-frame").remove ();
+    
+    var sectionEl = document.createElement("section");
+    sectionEl.className = "questions";
+    sectionEl.setAttribute("id", "question-frame");
+    
+    var questionTitle = document.createElement("h1");
+    questionTitle.textContent = questions[0].question;
+    sectionEl.appendChild(questionTitle);
+    mainEl.appendChild(sectionEl);
+  
+    var choicesPossible = document.createElement("ol");
+
+    var choicesPossible = document.createElement("li");
+    choicesPossible.textContent = questions[0].choices[0];
+    sectionEl.className = "questions";
+    sectionEl.setAttribute("id", "choices");
+    sectionEl.appendChild(choicesPossible);
+    mainEl.appendChild(sectionEl);
+
+    console.log(sectionEl);
+    console.log(questionTitle);
+    console.log(choicesPossible);
+
+  }
+};
+mainEl.addEventListener("click", startQuiz)
